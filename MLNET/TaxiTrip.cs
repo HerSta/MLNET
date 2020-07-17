27cd3e31ac161e80c495ee0010fff3be37b62d65
herman@penetrace.com <herman@penetrace.com>
2020-07-17 13:41:47 +0200

    Add project files.

---
+++ MLNET/TaxiTrip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace MLNET
+{
+   public class TaxiTrip
+    {
+        [LoadColumn(0)]
+        public string VendorId;
+        [LoadColumn(1)]
+        public string RateCode;
+        [LoadColumn(2)]
+        public string PassengerCount;
+        [LoadColumn(3)]
+        public string TrimTime;
+        [LoadColumn(4)]
+        public string TripDistance;
+        [LoadColumn(5)]
+        public string PaymentType;
+        [LoadColumn(6)]
+        public string FareAmount;
+    }
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
+
+
